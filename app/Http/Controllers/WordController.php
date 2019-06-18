fd367a18f260a495a59af4fe0cd062a95627abfc
Sampuccino <Sampuccino@gmail.com>
2019-06-17 21:55:47 -0700

    Initial commit.

---
+++ app/Http/Controllers/WordController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Word;
+use Illuminate\Http\Request;
+use App\Http\Helpers\OxfordApi;
+
+class WordController extends Controller
+{
+  private $oxford = null;
+
+  public function __construct() {
+    $this->oxford = new OxfordApi();
+  }
+
+  /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+      dd($this->oxford->callApi('ace'));
+//      return view('word.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Word  $word
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Word $word)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Word  $word
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Word $word)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Word  $word
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Word $word)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Word  $word
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Word $word)
+    {
+        //
+    }
+}
---
+++ resources/views/word/index.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+@section('content')
+  <div class="container d-flex flex-row justify-content-between">
+    <div class="p-2">Word 1</div>
+    <div class="p-2">Word 2</div>
+    <div class="p-2">Word 3</div>
+  </div>
+@stop
